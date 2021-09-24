c3b7ebc730a0d092495441b174fed62e896e4855
mlrosas21 <mlrosas21@hotmail.com>
2021-09-24 11:25:33 -0300

    clase 19 - falta punto H

--- clase19/app.js
+++ clase19/app.js
@@ -5,9 +5,9 @@ let arrayProfesionales = archivos.leerJson("profesionales");
 
 //A
 
-// let nombreEspecialidad = arrayProfesionales.forEach(profesional => console.log(profesional.nombre + ' - ' + profesional.especialidad))
+let nombreEspecialidad = arrayProfesionales.forEach(profesional => console.log(profesional.nombre + ' - ' + profesional.especialidad))
 
-// nombreEspecialidad
+nombreEspecialidad
 
 //B Utilizar un método de array que permita recorrer todo el arrayProfesionales y modificar de cada uno la propiedad honorarioConsulta incrementado el valor de la misma en un 5%, luego imprimir el arrayProfesionales para ver la modificación realizada.
 
@@ -17,16 +17,11 @@ console.log(honorarioConsulta5)
 
 //C Utilizar un método de array que permite recorrer todo el array de profesionales y retornar profesionales deshabilitados, es decir, filtrar por la propiedad estaHabilitado cuando esta sea false, guardar el retorno de este filtro en una variable profesionalesDeshabilitados e imprimir la misma para verificar los objetos filtrados.
 
-let profesionalesDeshabilitadosVariable = []
+console.log('------------------DESHABILITADOS-------------------------')
 
-function mostrarDeshabilitados(array) {
-    profesionalesDeshabilitados = array.filter(profesional => profesional.estaHabilitado === false)
-    profesionalesDeshabilitadosVariable = return profesionalesDeshabilitados
-}
+let profesionalesDeshabilitados = arrayProfesionales.filter(profesional => profesional.estaHabilitado === false)
 
-console.log(mostrarDeshabilitados(arrayProfesionales))
-
-console.log('VARIABLE PROFESIONALES DEHABILITADOS' + profesionalesDeshabilitados)
+console.log(profesionalesDeshabilitados)
 
 //D Utilizar un método de array que permite recorrer todo el array de profesionales y retornar profesionales habilitados, es decir, filtrar por la propiedad estaHabilitado cuando esta sea true, guardar el retorno de este filtro en una variable profesionaleHabilitados e imprimir la misma para verificar los objetos filtrados.
 
@@ -52,14 +47,54 @@ console.log(mostrarArquitectos(arrayProfesionales))
 
 //F Utilizar un método de array que permita recorrer todo el array y modificar de cada uno la propiedad estaHabilitado a true, se recomienda utilizar la variable obtenida del punto C, es decir la variable profesionalesDeshabilitados , imprimir el resultado de este método.
 
-console.log('--------------------------------HABILITANDO PROFESIONALES----------------------------------')
+console.log('--------------HABILITANDO PROFESIONALES------------')
 
 function habilitarProfesionales(array) {
-    array.map(profesionales => profesionales.estaHabilitado = true)
+    let nuevosProfesionalesHabilitados = array.map(profesionales => profesionales.estaHabilitado = true)
+    return nuevosProfesionalesHabilitados
 }
 
 console.log(habilitarProfesionales(profesionalesDeshabilitados))
 
-//G 
+//G Ahora necesitamos obtener un valor que nos indique la cantidad de consultas atendidas por todos los profesionales, para ello utilizaremos un método de array nos facilite esto, asignamos él retorno de este método en una variable llamada cantidadTotalConsultas, luego imprimir la misma.
 
-//H 
+console.log('------------------CONSULTAS-------------------')
+
+function consultasTotales(array) {
+    let sumaConsultas = array.reduce((suma, profesional) => suma + profesional.cantidadConsultas, 0)
+    return sumaConsultas
+}
+
+const cantidadTotalConsultas = consultasTotales(arrayProfesionales)
+
+console.log(`La suma total de consultas de todos los profesionales es ${cantidadTotalConsultas}`)
+
+/* Utilizar un método de array, que nos permita retornar un nuevo array de
+objetos literales únicamente con las propiedades nombre, especialidad y
+recaudación que será él resultado de multiplicar las propiedades
+cantidadConsulta * honorarioConsulta, asignar él retorno de este método a una
+variable arrayRecaudacion e imprimir la misma, la salida por terminal se debería
+ver como los siguiente:
+[
+{
+nombre: 'Huber Wilkins',
+especialidad: 'Neumonologia',
+recaudacion: 4015.36
+},
+{
+nombre: 'Goldie Haley',
+especialidad: 'Cardiologia',
+recaudacion: 9302.24
+},
+{
+nombre: 'Pena Landry',
+especialidad: 'Dermatologia',
+recaudacion: 0
+},
+…] */
+
+console.log('------------------NUEVO ARRAY-------------------')
+
+let recaudacion = arrayProfesionales.map(profesional => profesional.honorarioConsulta * profesional.cantidadConsultas)
+
+console.log(recaudacion)
